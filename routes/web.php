177687148d8abff36229362d9aa00d19a6ba317e
Deepak Rajpurohit <deepak.codoffer@gmail.com>
2024-11-08 11:53:30 +0530

    fluent string

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use App\Http\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+
+// $info = "hi, let's learn laravel";
+
+// // $info=Str::ucfirst($info);
+// // $info=Str::replaceFirst('Hi', 'hello', $info);
+// // $info=Str::camel($info);
+                                                  //fluent string
+// $info=Str::of($info)
+// ->ucfirst($info)
+// ->replaceFirst('Hi', 'hello', $info)
+// ->camel($info);
+
+// echo $info;
+
 
 Route::get('/', function () {
     return view('welcome');
